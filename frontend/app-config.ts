39e881c3163563b48ce63955c83ddd89d3479d22
Abhishek2110 <abhishekrajpal819@gmail.com>
2025-11-27 20:43:47 +0530

    Day 6 – Fraud Alert Voice Agent - Frontend

--- frontend/app-config.ts
+++ frontend/app-config.ts
@@ -29,11 +29,11 @@ export const APP_CONFIG_DEFAULTS: AppConfig = {
   supportsScreenShare: true,
   isPreConnectBufferEnabled: true,
 
-  logo: '/lk-logo.svg',
+  logo: '/au_logo.png',
   accent: '#002cf2',
-  logoDark: '/lk-logo-dark.svg',
-  accentDark: '#1fd5f9',
-  startButtonText: 'Start call',
+  logoDark: '/au_logo1.png',
+  accentDark: '#fd5f04',
+  startButtonText: 'Start Call',
 
   // for LiveKit Cloud Sandbox
   sandboxId: undefined,
