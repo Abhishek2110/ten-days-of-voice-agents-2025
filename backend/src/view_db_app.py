eea3786d240eb32fd688658ab496e3fc153b47fa
Abhishek2110 <abhishekrajpal819@gmail.com>
2025-11-27 20:23:22 +0530

    Day 6 – Fraud Alert Voice Agent - Backend

---
+++ backend/src/view_db_app.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+st.title("PS: Data is not real, Dummy Data Only")
+st.title("AU Small Finance Bank Fraud Cases")
+
+
+conn = sqlite3.connect("fraud_cases.db")
+df = pd.read_sql_query("SELECT * FROM fraud_cases", conn)
+conn.close()
+
+st.dataframe(df, use_container_width=True)
+
