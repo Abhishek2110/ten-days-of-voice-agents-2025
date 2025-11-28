551ab4b1b83bbc16fe513bacc8d18380e459eafe
Abhishek2110 <abhishekrajpal819@gmail.com>
2025-11-28 20:39:05 +0530

    Day 7 – Food & Grocery Ordering Voice Agent - Frontend

--- frontend/app-config.ts
+++ frontend/app-config.ts
@@ -20,20 +20,20 @@ export interface AppConfig {
 }
 
 export const APP_CONFIG_DEFAULTS: AppConfig = {
-  companyName: 'LiveKit',
-  pageTitle: 'LiveKit Voice Agent',
-  pageDescription: 'A voice agent built with LiveKit',
+  companyName: 'Flipkart',
+  pageTitle: 'FlipFresh',
+  pageDescription: 'Your personal shopping assistant powered by AI.',
 
   supportsChatInput: true,
   supportsVideoInput: true,
   supportsScreenShare: true,
   isPreConnectBufferEnabled: true,
 
-  logo: '/lk-logo.svg',
+  logo: '/logo1.png',
   accent: '#002cf2',
-  logoDark: '/lk-logo-dark.svg',
-  accentDark: '#1fd5f9',
-  startButtonText: 'Start call',
+  logoDark: '/logo1.png',
+  accentDark: '#ffffff',
+  startButtonText: 'Order Now',
 
   // for LiveKit Cloud Sandbox
   sandboxId: undefined,
