b349d04e04fdfe366bc75afcff06f92398b4ac6c
Abhishek2110 <abhishekrajpal819@gmail.com>
2025-11-30 15:02:52 +0530

    Day 9 – E-commerce Agent (Based on the Agentic Commerce Protocol)

--- frontend/components/app/welcome-view.tsx
+++ frontend/components/app/welcome-view.tsx
@@ -34,7 +34,7 @@ export const WelcomeView = ({
         <WelcomeImage />
 
         <p className="text-foreground max-w-prose pt-1 leading-6 font-medium">
-          Chat live with your voice AI agent
+          Talk to Ken - AI powered shopping assistant by Amazon.
         </p>
 
         <Button variant="primary" size="lg" onClick={onStartCall} className="mt-6 w-64 font-mono">
